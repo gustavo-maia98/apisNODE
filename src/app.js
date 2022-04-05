c18a70acd6d94d72e2e81e4a1c106d748d1ea09c
Gustavo Maia <gstv.maiaa@gmail.com>
2022-04-05 11:18:43 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+const app = express();
+const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect(config.connectionString);
+
+//Carrega os Models
+const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
+
+// Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/costumer-route');
+const orderRoute = require('./routes/order-route');
+const { connectionString } = require('./config');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
+
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://gustavo:gustavo@ndstr.e2csr.mongodb.net/ndstr?retryWrites=true&w=majority',
+    sendgridKey: 'gSG.DfIEznSrSdieMLFPeeTIDg.H2pjVmWzUjR7H6aQzCEJOcAIbjZP_-nuKVOishE7waE',
+    containerConnectionString: 'mongodb+srv://gustavo:gustavo@ndstr.e2csr.mongodb.net/ndstr?retryWrites=true&w=majority'
+}
